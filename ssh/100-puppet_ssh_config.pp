2488c8178730d5164adcae410d35ac5a32189ffd
ibenitha32 <b.iradukund1@alustudent.com>
2023-07-23 13:13:03 -0700

    extra task

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#don't use any password
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
