154633b687c6f6c2934454b6c2d4613743ee579c
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-08-27 23:37:27 +0200

    Task 0

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+#Let's test our server
+
+exec { 'ULIMIT':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
