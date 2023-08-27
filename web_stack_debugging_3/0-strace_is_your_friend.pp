191db620bb027393383c4d3ed3b67fbcf30f6193
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-08-27 23:36:02 +0200

    Task 0

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fixing apache
+exec { 'fixing typo...':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
